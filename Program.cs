7c9c15502d12ae3922862510a5bdd2e372c293c8
F0recast1254 <95315760+F0recast1254@users.noreply.github.com>
2023-11-13 16:00:34 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Template
+{
+    /*
+     * Spring 2023
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+            game.Play();
+            game.End();
+        }
+    }
+}
